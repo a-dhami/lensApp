21fe5bcc6d66f4815141b22ea3b84ecdf9bf91b6
akash <asd15@sfu.ca>
2020-01-16 17:20:15 -0800

    Created the Lens class and initialized constructor and getter/setters.

--- src/main/java/Model/Lens.java
+++ src/main/java/Model/Lens.java
@@ -1,4 +1,42 @@
+/**
+ * This class represents a Lens and the corrosponding information such as the make, max aperture, and the focal length
+ */
 package Model;
 
 public class Lens {
+    private String make;
+    private double maxAperture;
+    private double focalLength;
+
+    //auto generated constructor.
+    public Lens(String make, double maxAperture, double focalLength) {
+        this.make = make;
+        this.maxAperture = maxAperture;
+        this.focalLength = focalLength;
+    }
+
+    //auto generated getter and setter
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public double getMaxAperture() {
+        return maxAperture;
+    }
+
+    public void setMaxAperture(double maxAperture) {
+        this.maxAperture = maxAperture;
+    }
+
+    public double getFocalLength() {
+        return focalLength;
+    }
+
+    public void setFocalLength(double focalLength) {
+        this.focalLength = focalLength;
+    }
 }
