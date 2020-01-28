8e7305a3f0165595019a5d0dd4d4af8cf73a8701
akash <asd15@sfu.ca>
2020-01-27 18:15:10 -0800

    Removing unnecessary code from Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,13 +7,6 @@ public class Main {
     public static void main (String args[]) {
 
         LensManager manager = new LensManager();
-        //manager.add(new Lens("canon", 2.3, 4));
-        //manager.add(new Lens("Nikon", 2.2, 33));
-
-        //Lens l = manager.get(1);
-        //System.out.println(manager.get(1).getFocalLength());
-
-
 
         CameraTextUI ui = new CameraTextUI(manager);
         ui.show();
